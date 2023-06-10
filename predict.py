5268802c8c91650219c06d608a824b0e88e2b772
Paul Hayes <fofr@users.noreply.github.com>
2023-06-10 23:01:42 +0100

    Add further parameters
    Width, height, negative_prompt, guidance_scale

--- predict.py
+++ predict.py
@@ -4,9 +4,7 @@ from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 from cog import BasePredictor, Input, Path
 import imageio
 
-
 MODEL_CACHE = "model-cache"
-
 
 class Predictor(BasePredictor):
     def setup(self):
@@ -30,11 +28,23 @@ class Predictor(BasePredictor):
         prompt: str = Input(
             description="Input prompt", default="An astronaut riding a horse"
         ),
+        negative_prompt: str = Input(
+            description="Negative prompt", default=None
+        ),
         num_frames: int = Input(
             description="Number of frames for the output video", default=16
         ),
         num_inference_steps: int = Input(
             description="Number of denoising steps", ge=1, le=500, default=50
+        ),
+        width: int = Input(
+            description="Width of the output video", ge=256, default=256
+        ),
+        height: int = Input(
+            description="Height of the output video", ge=256, default=256
+        ),
+        guidance_scale: float = Input(
+            description="Guidance scale", ge=1.0, le=100.0, default=7.5
         ),
         fps: int = Input(description="fps for the output video", default=8),
         seed: int = Input(
@@ -50,6 +60,10 @@ class Predictor(BasePredictor):
             prompt,
             num_inference_steps=num_inference_steps,
             num_frames=num_frames,
+            width=width,
+            height=height,
+            negative_prompt=negative_prompt,
+            guidance_scale=guidance_scale,
             generator=generator,
         ).frames
 
